2fc3f1730d99fc78182bfadf289a46a19ae99c87
mk-nazemi <nazemi.works@gmail.com>
2019-09-05 09:24:06 +0430

    so well for second way of build component

--- src/main.js
+++ src/main.js
@@ -1,8 +1,42 @@
 import Vue from "vue";
-import App from "./App.vue";
+import wrap from '@vue/web-component-wrapper';
+import VueWebComponent from './components/VueWebComponent';
+
+
+
+const CustomElement = wrap(Vue, VueWebComponent);
+// The API for registering a web component is customElements.define()
+// "custom element" = "web component" 
+window.customElements.define('my-custom-element', CustomElement);
+
+
+
+
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
+    render: h => h(App)
 }).$mount("#app");
+
+// Object.keys(Components).forEach(name => {
+//   Vue.component(name, Components[name]);
+// });
+// const CustomElement = wrap(Vue, SafaGridView);
+// // The API for registering a web component is customElements.define()
+// // "custom element" = "web component" 
+// window.customElements.define('SafaGridView', CustomElement);
+
+
+
+// these  below line maybe need for load up comps.
+
+// 
+//      < script src = "https://unpkg.com/vue" > < /script> <
+//       !--Load the web component polyfill-- >
+//       <
+//       script src = "https://cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/1.2.0/webcomponents-loader.js" > < /script> <
+//       !--Load your custom element-- >
+//       <
+//       script src = "./my-custom-element.js" > < /script> 
+//
